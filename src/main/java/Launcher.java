66db9223ee3c5f322600a84c7864fe98c0ead44d
Cody Bumgardner <cody@uky.edu>
2019-07-19 19:17:13 -0400

    first post

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Launcher {
@@ -5,25 +7,59 @@ public class Launcher {
 
     public static void main(String args[]) {
 
-        Random r = new Random();
-        int low = 10;
-        int high = 100;
-
         System.out.println("height,x,y,set");
 
+        Random r = new Random();
+        int low = 0;
+        int high = 100;
+
         for(int s = 1; s <=25; s++) {
-            for (int y = 1; y <= 100; y++) {
 
-                for (int x = 1; x <= 100; x++) {
+            Map<String,Integer> pointMap = new HashMap<>();
 
-                    //int result = r.nextInt(high - low) + low;
-                    int result = r.nextInt(100 - y) + y;
+            for(int i = 0; i < 10000; i++) {
 
+                //int x = r.nextInt(high-low) + low;
+                //int y = r.nextInt(high-low) + low;
+                int x = r.nextInt(high-low) + low;
+                int y = r.nextInt(high-low) + low;
 
-                    System.out.println(result + "," + x + "," + y + "," + s);
+                String point = x + "," + y;
+
+                if(pointMap.containsKey(point)) {
+                    pointMap.put(point,pointMap.get(point) + 1);
+                } else {
+                    pointMap.put(point,1);
                 }
 
             }
+
+            for(int y = 1; y <= 100; y++) {
+
+                for (int x = 1; x <= 100; x++) {
+
+                    int result = 0;
+                    String point = x + "," + y;
+                    if(pointMap.containsKey(point)) {
+                        result = pointMap.get(point);
+                    }
+                        System.out.println(result + "," + x + "," + y + "," + s);
+                }
+
+            }
+
+            /*
+            for (Map.Entry<String, Integer> entry : pointMap.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                System.out.println(value + "," + key + "," + s);
+            }
+            */
+
+
+
         }
+
+
     }
 }
