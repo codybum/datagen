ad83a7084473feea8d28d0bbb0e647b2041efb07
Cody Bumgardner <cody@uky.edu>
2019-07-19 18:44:15 -0400

    Initial commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Launcher {
+
+
+    public static void main(String args[]) {
+
+        Random r = new Random();
+        int low = 10;
+        int high = 100;
+
+        System.out.println("height,x,y,set");
+
+        for(int s = 1; s <=2; s++) {
+            for (int y = 1; y <= 61; y++) {
+
+                for (int x = 1; x <= 87; x++) {
+                    int result = r.nextInt(high - low) + low;
+
+                    System.out.println(result + "," + x + "," + y + "," + s);
+                }
+
+            }
+        }
+    }
+}
